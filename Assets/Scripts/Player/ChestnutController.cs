0234ed4875c45c17980770ee700e3f383bb88a95
NathanielFeng <Js.Nat.Feng@gmail.com>
2020-04-08 20:38:09 +0800

    Revert "Merge branch 'master' of https://github.com/NathanielFeng/2D-Game"
    This reverts commit 07799f2706cb2f4e4eadf07e0455befe60b7c706, reversing
    changes made to 10919981dc4a6a18433d7e4c39f594338dd62dd3.

--- Assets/Scripts/Player/ChestnutController.cs
+++ Assets/Scripts/Player/ChestnutController.cs
@@ -6,11 +6,8 @@ public class ChestnutController : MonoBehaviour
 {
     private Rigidbody2D rigid;
     private Animator anim;
-    private CircleCollider2D circleCollider;
-    public LayerMask ground;
 
     public float input;
-    public float jumpForce = 8.0f;
     private float faceDirection;
     public float speed = 3.0f;
     public string transformKey;
@@ -19,7 +16,6 @@ public class ChestnutController : MonoBehaviour
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        circleCollider = GetComponent<CircleCollider2D>();
     }
     // Start is called before the first frame update
     void Start()
@@ -33,33 +29,15 @@ public class ChestnutController : MonoBehaviour
         input = Input.GetAxis("Horizontal");
         faceDirection = Input.GetAxisRaw("Horizontal");
 
-        //变身后滚动
-        if (anim.GetBool("isMature"))  
-            anim.SetBool("isRolling", (input != 0 ? true : false));
-        //变身前跑动
-        else
-            anim.SetBool("isBouncing", (input != 0 ? true : false));
-        //是否变身
-        if (!anim.GetBool("isMature") && !anim.GetBool("isBouncing") && Input.GetKeyDown(transformKey)) 
-        {
-            anim.SetBool("isMature", true);
-            speed *= 2;
-            //改变Circle Collider碰撞体的大小
-            circleCollider.offset = new Vector2(0.01f, 1.0f);
-            circleCollider.radius = 1.0f;
-        }     
- 
+        //变身
+        if(Input.GetKeyDown(transformKey))
+            anim.SetBool("transform", true);
     }
 
     void FixedUpdate()
     {
-        //移动
         rigid.velocity = new Vector2(input * speed, rigid.velocity.y);
-        //朝向
-        if (faceDirection != 0)
+        if(faceDirection != 0)
             transform.localScale = new Vector3(faceDirection, 1, 1);
-        //跳起
-        if (Input.GetButtonDown("Jump") && circleCollider.IsTouchingLayers(ground) && anim.GetBool("isRolling"))
-            rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
     }
 }
