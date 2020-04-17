e2b8d303f88617a4c27309a9f04f7b711009b53a
banbianzhang <845855641@qq.com>
2020-04-18 04:35:04 +0800

    删除王宫场景，修复动作bug，添加脚步声

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -23,6 +23,11 @@ public class PlayerController : MonoBehaviour
     public float m_floatingWindow;
     public float m_attackIdle;
 
+    [Space]
+    //Sound
+    public AudioClip m_soundFootstepsGrass;
+    public AudioClip m_soundFootstepsStone;
+
     //Player private info
     private bool m_isJumping;
     private bool m_isFloating;
@@ -34,9 +39,12 @@ public class PlayerController : MonoBehaviour
     private float m_posRecY;
     private float m_posRecX;
     private float m_gravityRec;
-    private float m_attackIdleCnt;
+    private float m_attackTimeCnt;
+    private float m_dashTimeCnt;
     private bool m_nextJump;
     private bool m_isOnGround;
+    private bool m_nextDash;
+    private string m_groundTag;
 
 
     // Start is called before the first frame update
@@ -53,6 +61,7 @@ public class PlayerController : MonoBehaviour
         Attack();
         GroundCheck();
         Dash();
+        AudioControl();
         //更新生命值
         m_slider.value = m_health;
     }
@@ -66,8 +75,9 @@ public class PlayerController : MonoBehaviour
     {
         m_rb = GetComponent<Rigidbody2D>();
         m_anim = GetComponent<Animator>();
-        m_attackIdleCnt = 0;
+        m_attackTimeCnt = 0;
         m_nextJump = true;
+        m_nextDash = true;
     }
 
     // Control player's movement
@@ -77,13 +87,13 @@ public class PlayerController : MonoBehaviour
         float horizontalMove = Input.GetAxis("Horizontal");
         float faceDirection = Input.GetAxisRaw("Horizontal");
 
-        //Turn around
+        //转身判定
         if (faceDirection != 0)
         {
             float formerDirection = transform.localScale.x;
-            //Set new direction
+            //设置新的方向
             transform.localScale = new Vector3(faceDirection, 1, 1);
-            //If player face direction is changed
+            //比较新方向和旧方向判断玩家是否转向
             if (formerDirection == transform.localScale.x)
             {
                 m_anim.SetBool("Turning", false);
@@ -103,8 +113,8 @@ public class PlayerController : MonoBehaviour
     //Player jump
     void Jump()
     {
-        //Prevents player hold the key and jump
-        if(Input.GetButtonUp("Jump"))
+        //防止按住跳跃键连跳，必须松开再按才能跳
+        if (Input.GetButtonUp("Jump"))
         {
             m_nextJump = true;
         }
@@ -115,7 +125,7 @@ public class PlayerController : MonoBehaviour
             m_anim.SetBool("Falling", true);
         }
         //Jump
-        if (Input.GetButton("Jump") && /*m_coll.IsTouchingLayers(m_ground)*/m_isOnGround && !m_isAttacking 
+        if (Input.GetButton("Jump") && m_isOnGround && !m_isAttacking 
             && !m_isFalling && !m_isFloating && m_nextJump) 
         {
             m_rb.velocity = new Vector2(m_rb.velocity.x, m_jumpForce);
@@ -127,6 +137,7 @@ public class PlayerController : MonoBehaviour
         else if (m_isJumping && ((m_rb.velocity.y < m_floatingWindow && m_rb.velocity.y > -1.0 * m_floatingWindow)
             || Input.GetButtonUp("Jump")))
         {
+            //if jump button up, stop jumping
             if(!(m_rb.velocity.y < m_floatingWindow && m_rb.velocity.y > -1.0 * m_floatingWindow))
             {
                 m_rb.velocity = new Vector2(m_rb.velocity.x, m_floatingWindow);
@@ -145,10 +156,19 @@ public class PlayerController : MonoBehaviour
             m_anim.SetBool("Falling", true);
         }
         //On the ground
-        else if (m_isFalling /*&& m_coll.IsTouchingLayers(m_ground) */&& m_isOnGround)
+        //必须添加纵向速度为0作为二号检测条件，否则起跳一瞬间也会判定on the ground
+        if (m_isOnGround)
         {
-            m_isFalling = false;
-            m_anim.SetBool("Falling", false);
+            m_nextDash = true;
+            if (m_isJumping || m_isFloating)
+            {
+                m_anim.SetTrigger("OnGround");
+            }
+            if (m_isFalling)
+            {
+                m_isFalling = false;
+                m_anim.SetBool("Falling", false);
+            }
             //Reset Attack
             if (m_anim.GetBool("AttackingDown"))
             {
@@ -162,11 +182,11 @@ public class PlayerController : MonoBehaviour
     //Player Attack
     void Attack()
     {
-        if (m_attackIdleCnt <= 0)
+        if (m_attackTimeCnt <= 0)
         {
             if (Input.GetButtonDown("Fire1") && !m_anim.GetBool("Attacking"))
             {
-                m_attackIdleCnt = m_attackIdle;
+                m_attackTimeCnt = m_attackIdle;
                 if (Input.GetButton("Vertical"))
                 {
                     if (Input.GetAxisRaw("Vertical") < 0)
@@ -200,7 +220,8 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            m_attackIdleCnt -= Time.deltaTime;
+            m_attackTimeCnt -= Time.deltaTime;
+            //Combo Attack
             if (Input.GetButtonDown("Fire1"))
             {
                 m_anim.SetTrigger("Attacking2");
@@ -234,8 +255,10 @@ public class PlayerController : MonoBehaviour
 
     void Dash()
     {
-        if (Input.GetButtonDown("Dash"))
+        if (Input.GetButtonDown("Dash") && m_nextDash && m_dashTimeCnt <= 0)
         {
+            m_dashTimeCnt = 1.0f;
+            m_nextDash = false;
             int directionSymbol = 0;
             float direction = transform.localScale.x;
             if(transform.localScale.x > 0)
@@ -258,6 +281,10 @@ public class PlayerController : MonoBehaviour
             m_rb.gravityScale = 0;//No gravity effect while dashing
             m_rb.velocity = new Vector2(directionSymbol * m_dashForce, 0);
         }
+        else
+        {
+            m_dashTimeCnt -= Time.deltaTime;
+        }
         if (m_isDashing && (Mathf.Abs(transform.position.x - m_posRecX) > m_dashDistance))
         {
             DashFinished();
@@ -267,8 +294,17 @@ public class PlayerController : MonoBehaviour
     void DashFinished()
     {
         m_anim.SetBool("Dash", false);
+        if (m_isDashing && (m_isJumping || m_isFloating || m_isFalling))
+        {
+            m_isJumping = false;
+            m_isFloating = false;
+            m_isFalling = true;
+            m_anim.SetBool("Jumping", false);
+            m_anim.SetBool("Floating", false);
+            m_anim.SetBool("Falling", true);
+        }
         m_isDashing = false;
-        m_rb.velocity = new Vector2(m_velocityRecX, m_velocityRecY);
+        m_rb.velocity = new Vector2(m_velocityRecX, 0);
         m_rb.gravityScale = m_gravityRec;
     }
 
@@ -277,25 +313,26 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 pos = transform.position;
         BoxCollider2D boxColl = (BoxCollider2D)m_coll;
-        Vector2 rightOffset = new Vector2(boxColl.size.x / 2, -boxColl.size.y / 2);
-        Vector2 leftOffset = new Vector2(-boxColl.size.x / 2, -boxColl.size.y / 2);
-        float distance = 0.2f;
+        Vector2 rightOffset = new Vector2(boxColl.size.x / 2, -boxColl.size.y / 2 );
+        Vector2 leftOffset = new Vector2(-boxColl.size.x / 2, -boxColl.size.y / 2 );
+        float distance = 0.1f;
 
-        RaycastHit2D rightFoot = Physics2D.Raycast(pos + rightOffset + m_coll.offset, Vector2.down, distance, m_ground);
-        RaycastHit2D leftFoot = Physics2D.Raycast(pos + leftOffset + m_coll.offset, Vector2.down, distance, m_ground);
+        RaycastHit2D rightFoot = Physics2D.Raycast(pos + rightOffset + boxColl.offset, Vector2.down, distance, m_ground);
+        RaycastHit2D leftFoot = Physics2D.Raycast(pos + leftOffset + boxColl.offset, Vector2.down, distance, m_ground);
 
         //Debug
-        //Color c = Color.red;
-        //if (rightFoot || leftFoot)
-        //{
-        //    c = Color.green;
-        //}
-        //Debug.DrawRay(pos + rightOffset + m_coll.offset, Vector2.down, c, distance);
-        //Debug.DrawRay(pos + leftOffset + m_coll.offset, Vector2.down, c, distance);
+        Color c = Color.red;
+        if (rightFoot || leftFoot)
+        {
+            c = Color.green;
+        }
+        Debug.DrawRay(pos + rightOffset + boxColl.offset, Vector2.down, c, distance);
+        Debug.DrawRay(pos + leftOffset + boxColl.offset, Vector2.down, c, distance);
 
         if (rightFoot || leftFoot)
         {
             m_isOnGround = true;
+            m_groundTag = rightFoot.collider.tag;
         }
         else
         {
@@ -303,9 +340,27 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void WallCheck()
+    void AudioControl()
     {
-
+        AudioSource source = GetComponent<AudioSource>();
+        //播放脚步声
+        if (m_isOnGround && Input.GetButton("Horizontal") && !source.isPlaying)
+        {
+            if (m_groundTag == "Grass")
+            {
+                source.clip = m_soundFootstepsGrass;
+            }
+            else if (m_groundTag == "Stone")
+            {
+                source.clip = m_soundFootstepsStone;
+            }
+            source.loop = true;
+            source.Play(0);
+        }
+        else if (!m_isOnGround || m_isAttacking || m_isDashing || Input.GetButtonUp("Horizontal"))
+        {
+            source.Stop();
+        }
     }
 
     public void takeDamage(float damage)
