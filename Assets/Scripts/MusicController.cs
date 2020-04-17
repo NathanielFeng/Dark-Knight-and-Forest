e0a2efd324196956e66b5eca0dccf7f454baa979
banbianzhang <845855641@qq.com>
2020-04-17 19:01:14 +0800

    添加了forest1的背景和BGFM

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController: MonoBehaviour
+{
+    public Rigidbody playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(playerRB.transform.position.x, playerRB.transform.position.y);
+    }
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float moveRate;
+    public Camera cam;
+    private float startPosX;
+    public float boundRight;
+    public float boundleft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float modPos = cam.transform.position.x * moveRate;
+        if (modPos < boundRight && modPos > boundleft)
+        {
+            transform.position = new Vector2(startPosX + modPos, transform.position.y);
+        }
+    }
+}
