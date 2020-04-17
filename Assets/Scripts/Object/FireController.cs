cac7f330779fff0fe55be4468f4eeb78e3c862c7
NathanielFeng <Js.Nat.Feng@gmail.com>
2020-04-17 17:24:09 +0800

    火焰定时喷射的代码实现

---
+++ Assets/Scripts/Object/FireController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    private Animator anim;
+    private float burstTime = 5.0f;
+    private float dormantTime = 1.0f;
+    private float timeCounter;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        anim.SetBool("BurstORFade", true);
+        timeCounter = burstTime;
+    }
+
+    void Update()
+    {
+        timeCounter -= Time.deltaTime;
+        if (timeCounter <= 0)
+        {
+            bool status = anim.GetBool("BurstORFade");
+            timeCounter = !status ? burstTime : dormantTime;
+            anim.SetBool("BurstORFade", !status);
+        }
+    }
+}
