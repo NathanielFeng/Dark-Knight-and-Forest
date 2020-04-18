325e171a0525b00bcc0b14ded69f68466a907047
NathanielFeng <Js.Nat.Feng@gmail.com>
2020-04-18 18:39:49 +0800

    增加了暂停菜单
    暂停菜单有两个bug。1：点击暂停菜单时人物仍能攻击；2：点击暂停菜单中的返回主菜单按钮后，回到主菜单，接着又点击主菜单的开始游戏按钮，进入游戏后人物无法动，只有点下暂停按钮后再点返回游戏按钮才恢复正常。

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(false);   
+    }
+
+    public void pauseMenu()
+    {
+        gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void returnGame()
+    {
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void returnMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
