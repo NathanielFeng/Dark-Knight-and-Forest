9969cffef57b77a3b00d47c8d189d1b9f517592f
NathanielFeng <Js.Nat.Feng@gmail.com>
2020-04-19 23:50:30 +0800

    将蝙蝠移植到Forest1并已完善

--- Assets/Scripts/Player/BatController.cs
+++ Assets/Scripts/Player/BatController.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BatController : MonoBehaviour
+public class BatController : Enemy
 {
     [Header("Attributes")]
-    public float health = 100;  //生命值100
     public float attackRange;   //攻击范围
     public float minDistance;   //和player保持的最小距离
     public float flySpeed;
     public float timeBtwShot;   //攻击间隔
     private float timeCounterBtwShot;   //攻击间隔计时
+    private float fallingSpeed = 5f;
 
     [Header("Components")]
     public Transform player;   //获取黑骑士的坐标
@@ -23,28 +23,39 @@ public class BatController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        health = 100;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, player.position) < attackRange) 
+        if (health > 0)
+            Attack();
+        else
+        {
+            anim.SetBool("isDead", true);
+            if(transform.position.y > -3.5)
+                transform.position += Vector3.down * fallingSpeed * Time.deltaTime;
+        }
+
+    }
+
+    void Attack()
+    {
+        if (Vector2.Distance(transform.position, player.position) < attackRange)
         {
             anim.SetBool("isAttacking", true);
             //朝player方向移动
             if (Vector2.Distance(transform.position, player.position) > minDistance)
-                    transform.Translate((player.position - transform.position) * flySpeed * Time.deltaTime);
+                transform.Translate((player.position - transform.position) * flySpeed * Time.deltaTime);
             //攻击
             if (timeCounterBtwShot <= 0)
             {
                 Vector3 difference = player.transform.position - transform.position;
                 float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg + 92.0f;   //搞不好了，只能手动加个92度
                 rotZ += Random.Range(-10.0f, 10.0f);
-                Instantiate(tooth, shotPoint.position, Quaternion.Euler(0f,0f,rotZ));
+                Instantiate(tooth, shotPoint.position, Quaternion.Euler(0f, 0f, rotZ));
                 timeCounterBtwShot = timeBtwShot;
             }
             else
@@ -54,7 +65,5 @@ public class BatController : MonoBehaviour
         {
             anim.SetBool("isAttacking", false);
         }
-
-
     }
 }
